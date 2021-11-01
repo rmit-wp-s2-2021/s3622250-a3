927c6da5af479acda92deb6215306b767cf39146
s3622250 <s3622250@student.rmit.edu.au>
2021-11-02 00:09:01 +1100

    Form Validation
    Added form validation for Product.php

--- a3/product.php
+++ a3/product.php
@@ -1,3 +1,37 @@
+<?php
+$prodname_error = $prodcat_error = $prodprice_error = "";
+if($_SERVER['REQUEST_METHOD'] == "POST")
+{
+    $prodname = $_POST['prod-name'];
+    $prodcat = $_POST['prod-type'];
+    $prodprice = $_POST['prod-price'];
+    $valid = true;
+    if(empty($prodname))
+    {
+        $valid = false;
+        $prodname_error = "Product name is required";
+    }
+    if(empty($prodcat))
+    {  
+        $prodcat_error = "Product Category is required";
+    }
+    else if(!preg_match("/^Beverages$/", $prodcat))
+    {
+        $prodcat_error = "Product Category has to be Beverages";
+    }
+
+    if(!preg_match('/\.\d{2,}/', $prodprice))
+    {
+        $prodprice_error = "Product Price must be 2 decimal places";
+    }
+    
+}
+
+
+?>
+
+
+
 <html lang="en">
 <head>
     <meta charset="UTF-8">
@@ -11,11 +45,17 @@
         <section>
             <form class="product-form" method="post">
                 <div>
+                    <p class="error"><?php echo $prodname_error; ?></p>
+                </div>
+                <div>
                     <label for="prod-name">Name:</label>
                     <input type="text" name="prod-name">
                 </div>
                 <div>
-                    <label for="prod-type">Product Type:</label>
+                    <p class="error"><?php echo $prodcat_error; ?></p>
+                </div>
+                <div>
+                    <label for="prod-type">Product Category:</label>
                     <input type="text" name="prod-type">
                 </div>
                 <div>
@@ -25,6 +65,9 @@
                         <option value="Exotic Liquids">Exotic Liquids</option>
                         <option value="HotNCold">HotNCold</option>
                     </select>
+                </div>
+                <div>
+                    <p class="error"><?php echo $prodprice_error; ?></p>
                 </div>
                 <div>
                     <label for="prod-price">Product Price:</label>
